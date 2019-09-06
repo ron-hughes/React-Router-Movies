c17e7f5bf1afd6734bbf9ee0820e0c6d626a6cc0
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2019-09-05 23:59:43 -0400

    completed mvp with stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 
+import {Route} from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
 import SavedList from './Movies/SavedList';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
+    (!savedList.includes(movie))&&
     setSavedList( [...savedList, movie] );
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
+      <Route path='/' exact component={MovieList} />
+      <Route path='/movies/:id' exact render={ (props) => ( <Movie addToSavedList={addToSavedList} {...props}/>)}/>
+      </div>
   );
 };
 
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
- 
+  const [movie, setMovie] = useState();
+ console.log(props)
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
+    
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,13 +19,13 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -49,7 +50,7 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
 }
